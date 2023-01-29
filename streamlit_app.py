39eb32bd9474ede5a357dc4ace3803d1bad3b3db
evolvPy <84336817+evolvPy@users.noreply.github.com>
2023-01-29 13:07:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('First Application')
 streamlit.header('Hello World')
 streamlit.text('Outside it is cloudy in Troy MI')
-streamlit.text('Snwo has stopped')
+streamlit.text('Snow has stopped')
 streamlit.text('Next week Jan-30 to Feb-07 will be freezing cold')
