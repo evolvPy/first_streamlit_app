95a4cd7de57d3f185efc2e023848b647ec1fc0c1
evolvPy <84336817+evolvPy@users.noreply.github.com>
2023-01-29 13:12:53 -0500

    Update streamlit_app.py
    Added datafarem

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('First Application')
 streamlit.header('Hello World')
@@ -8,3 +9,6 @@ streamlit.text('🥗 Next week Jan-30 to Feb-07 will be freezing cold')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
