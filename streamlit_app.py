b40dd1a9aff041fab42af20f0e140702b82f9610
evolvPy <84336817+evolvPy@users.noreply.github.com>
2023-01-29 13:10:57 -0500

    Update streamlit_app.py
    Added fruit smoothie  header

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.header('Hello World')
 streamlit.text('🥣 Outside it is cloudy in Troy MI')
 streamlit.text('🐔 Snow has stopped')
 streamlit.text('🥗 Next week Jan-30 to Feb-07 will be freezing cold')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
