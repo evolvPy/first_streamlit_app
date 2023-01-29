97dc20323f37f16bca204b5213e2092e0749f21b
evolvPy <84336817+evolvPy@users.noreply.github.com>
2023-01-29 13:10:13 -0500

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('First Application')
 streamlit.header('Hello World')
-streamlit.text('Outside it is cloudy in Troy MI')
-streamlit.text('Snow has stopped')
-streamlit.text('Next week Jan-30 to Feb-07 will be freezing cold')
+streamlit.text('🥣 Outside it is cloudy in Troy MI')
+streamlit.text('🐔 Snow has stopped')
+streamlit.text('🥗 Next week Jan-30 to Feb-07 will be freezing cold')
